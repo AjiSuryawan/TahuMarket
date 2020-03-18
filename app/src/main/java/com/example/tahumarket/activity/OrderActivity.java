96ed2ee6a999cfe35410bfe51c9831a56a2697a7
ryandhikabaa <dhika2018rplrus2@gmail.com>
2020-03-18 22:47:17 +0700

    update

--- app/src/main/java/com/example/tahumarket/activity/OrderActivity.java
+++ app/src/main/java/com/example/tahumarket/activity/OrderActivity.java
@@ -2,20 +2,26 @@ package com.example.tahumarket.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import com.example.tahumarket.R;
 
 public class OrderActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private LinearLayout divTambahDataOrder, divSinkronData;
+    private RecyclerView rvDaftarOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        binding();
 
         //set orientation
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
@@ -33,5 +39,33 @@ public class OrderActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void binding(){
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Data Order");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            private void doNothing() {
+
+            }
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        divSinkronData = findViewById(R.id.divSinkronData);
+        divSinkronData.setOnClickListener(new View.OnClickListener() {
+            private void doNothing() {
+
+            }
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        rvDaftarOrder = findViewById(R.id.rvDaftarOrder);
     }
 }
--- app/src/main/java/com/example/tahumarket/activity/ProdukActivity.java
+++ app/src/main/java/com/example/tahumarket/activity/ProdukActivity.java
@@ -2,27 +2,41 @@ package com.example.tahumarket.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.tahumarket.R;
 
 public class ProdukActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private EditText etSearch;
+    private ImageView ivSearch;
+    private LinearLayout divSinkronData;
+    private RecyclerView rvDaftarProduk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produk);
+        binding();
 
         //set orientation
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         //hide keyboard
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+
+    }
+
+    private void binding(){
+        toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Data Produk");
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             private void doNothing() {
@@ -34,5 +48,30 @@ public class ProdukActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        etSearch = findViewById(R.id.etSearch);
+        ivSearch = findViewById(R.id.ivSearch);
+        ivSearch.setOnClickListener(new View.OnClickListener() {
+            private void doNothing() {
+
+            }
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        divSinkronData = findViewById(R.id.divSinkronData);
+        divSinkronData.setOnClickListener(new View.OnClickListener() {
+            private void doNothing() {
+
+            }
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        rvDaftarProduk = findViewById(R.id.rvDaftarProduk);
     }
 }
