4dc628a58f674197caecd50968d2077a36fe9f9f
Aji Suryawan <kazumovie132@gmail.com>
2020-03-26 08:15:17 +0700

    adding arraylist

---
+++ app/src/main/java/com/example/tahumarket/model/NotaModel.java
@@ -0,0 +1,76 @@
+package com.example.tahumarket.model;
+
+public class NotaModel {
+    private String kodeBarang;
+    private String namaBarang;
+    private int hargaBarang;
+    private String kodeWarna;
+    private String kodePackaging;
+    private String kodeNota;
+    private String jumlahbarang;
+    private String subtotal;
+
+    public String getKodeBarang() {
+        return kodeBarang;
+    }
+
+    public void setKodeBarang(String kodeBarang) {
+        this.kodeBarang = kodeBarang;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    public int getHargaBarang() {
+        return hargaBarang;
+    }
+
+    public void setHargaBarang(int hargaBarang) {
+        this.hargaBarang = hargaBarang;
+    }
+
+    public String getKodeWarna() {
+        return kodeWarna;
+    }
+
+    public void setKodeWarna(String kodeWarna) {
+        this.kodeWarna = kodeWarna;
+    }
+
+    public String getKodePackaging() {
+        return kodePackaging;
+    }
+
+    public void setKodePackaging(String kodePackaging) {
+        this.kodePackaging = kodePackaging;
+    }
+
+    public String getKodeNota() {
+        return kodeNota;
+    }
+
+    public void setKodeNota(String kodeNota) {
+        this.kodeNota = kodeNota;
+    }
+
+    public String getJumlahbarang() {
+        return jumlahbarang;
+    }
+
+    public void setJumlahbarang(String jumlahbarang) {
+        this.jumlahbarang = jumlahbarang;
+    }
+
+    public String getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(String subtotal) {
+        this.subtotal = subtotal;
+    }
+}
